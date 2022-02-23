5e2c4db26962e00c09ae4e2bdb8fb0b343fe7380
Sarah <mansahmensah18@gmail.com>
2022-02-22 21:00:12 -0800

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+
+// import Vue from 'vue'
+
+// import App from '.vue/App.vue'
+
+import { createApp } from 'vue'
+import App from './vue/App.vue'
+
+createApp(App).mount('#app')
